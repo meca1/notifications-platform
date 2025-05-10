2e729273619be89c97f7b552d15dd3d0b4af7e08
meca1 <drramosdev@gmail.com>
2025-05-10 15:26:40 -0500

    feat: extend ISubscriptionRepository with new methods for finding subscriptions by client ID and updating existing subscriptions

--- src/core/ports/output/ISubscriptionRepository.ts
+++ src/core/ports/output/ISubscriptionRepository.ts
@@ -2,5 +2,7 @@ import { Subscription } from '../../domain/models/Subscription';
 
 export interface ISubscriptionRepository {
   findByClientIdAndEventType(clientId: string, eventType: string): Promise<Subscription | null>;
+  findByClientId(clientId: string): Promise<Subscription[]>;
   save(subscription: Subscription): Promise<void>;
+  update(clientId: string, eventType: string, subscription: Partial<Subscription>): Promise<void>;
 } 
